dd481d3b12a93ce1694696eed1220e5c20949dc5
Safi <saphidev@gmail.com>
2023-04-08 19:35:36 +0200

    clean

--- main.js
+++ main.js
@@ -1,18 +1,8 @@
 const core = require("@actions/core");
 const glob = require("@actions/glob");
-const fs = require("fs");
+const fs = require("fs/promises");
 const path = require("path");
 const sgMail = require("@sendgrid/mail");
-
-async function getAttachments(attachments) {
-  const globber = await glob.create(attachments.split(",").join("\n"));
-  const files = await globber.glob();
-  return files.map((f) => ({
-    filename: path.basename(f),
-    path: f,
-    content_id: f.replace(/^.*[\\\/]/, ""),
-  }));
-}
 
 async function sendMail() {
   const sgApiKey = core.getInput("sgApiKey", { required: true });
@@ -20,17 +10,14 @@ async function sendMail() {
   const from = core.getInput("from", { required: true });
   const to = core.getInput("to", { required: true });
   const text = core.getInput("text", { required: false });
-  const attachments = core.getInput("attachments", { required: false });
-
 
   sgMail.setApiKey(sgApiKey)
 
-  sgMail.send({
+  return sgMail.send({
     subject,
     from,
     to,
     text,
-    attachments: await getAttachments(attachments)
   })
 }
 
